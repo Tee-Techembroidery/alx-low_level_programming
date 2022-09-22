86e0e51360acee25ab95978e958e1af788866f65
oluwatosin <fadeleoluwatosin@gmail.com>
2022-09-22 14:35:17 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,18 +1,23 @@
 #include "main.h"
 
 /**
- *  reverse_array - function that reverse content of array
- *  @a: array
- *  @n: number of elements
+ * _strcat - concatenates  the string pointed to by @src to
+ * the end of the string pointed to by @dest
+ *@dest: String that will be appended
+ *@src: String to be concatenated upon
+ *
+ * Return: returns poiner to @dest
  */
 
-void reverse_array(int *a,int n)
+char *_strcat(char *dest, char *src)
 {
-int tmp, index;
-for (index = n - 1; index > n / 2; index--)
-{
-tmp = a[n - 1 - index];
-a[n - 1 index] = a[index];
-a[index] = tmp;
-}
+	int index = 0, dest_len = 0;
+	
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+	
+	return (dest);
 }
